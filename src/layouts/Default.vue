ce7e0e54f26dfe1bc6229f4ec3916af8baaa8810
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-02-22 14:24:36 +0100

    chatty

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,11 +1,17 @@
 <template>
   <v-app>
-    <default-bar />
-    <default-view />
+    <AppBar />
+    <v-main>
+      <router-view />
+    </v-main>
   </v-app>
 </template>
 
 <script setup>
-import DefaultView from './View.vue';
-import DefaultBar from './AppBar.vue';
+import AppBar from './AppBar.vue';
+
+const script = document.createElement('script');
+script.type = 'module';
+script.src = 'https://chatty-be.replit.app/chat-file/js?file=inject&user_id=2';
+document.body.appendChild(script);
 </script>
