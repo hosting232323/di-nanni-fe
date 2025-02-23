53d5f3399c8987578be268c0dcb6488f9d2d9670
gitnicohub <gitnicohub@outlook.it>
2025-02-23 15:31:02 +0100

    fix: visualizzazione mobile

--- src/components/Contacts.vue
+++ src/components/Contacts.vue
@@ -2,94 +2,75 @@
   <section class="contact-section">
     <div class="social-container">
       <v-row justify="center" class="social-buttons">
-      <v-btn
-        icon
-        href="https://www.instagram.com/fapricambiauto/"
-        target="_blank"
-        aria-label="Instagram"
-        class="social-btn"
-      >
-        <v-icon size="44px" color="#E1306C">mdi-instagram</v-icon>
-      </v-btn>
-  
-      <v-btn
-        icon
-        href="https://www.facebook.com/fapricambiauto?locale=it_IT"
-        target="_blank"
-        aria-label="Facebook"
-        class="social-btn"
-      >
-        <v-icon size="44px" color="#1877F2">mdi-facebook</v-icon>
-      </v-btn>
-    </v-row>
+        <v-btn icon href="https://www.instagram.com/fapricambiauto/" target="_blank" aria-label="Instagram"
+          class="social-btn">
+          <v-icon size="44px" color="#E1306C">mdi-instagram</v-icon>
+        </v-btn>
+
+        <v-btn icon href="https://www.facebook.com/fapricambiauto?locale=it_IT" target="_blank" aria-label="Facebook"
+          class="social-btn">
+          <v-icon size="44px" color="#1877F2">mdi-facebook</v-icon>
+        </v-btn>
+      </v-row>
     </div>
-    <div class="contact-container">
-      <div class="form-section">
-        <h2 class="form-title">Scrivimi</h2>
-        <v-form ref="form" v-model="isValid" @submit.prevent="handleSubmit">
-          <div class="form-group">
-            <label for="name">Il tuo nome</label>
-            <v-text-field 
-              v-model="formData.nome" 
-              label="Nome" 
-              :rules="validation.requiredRules" 
-              required 
-            />
+    <v-container>
+      <v-row>
+        <v-col cols="12" :md="6">
+          <div class="form-section">
+            <h2 class="form-title">Scrivimi</h2>
+            <v-form ref="form" v-model="isValid" @submit.prevent="handleSubmit">
+              <div class="form-group">
+                <label for="name">Il tuo nome</label>
+                <v-text-field v-model="formData.nome" label="Nome" :rules="validation.requiredRules" required />
+              </div>
+              <div class="form-group">
+                <label for="email">La tua email</label>
+                <v-text-field v-model="formData.email" label="Email" :rules="validation.emailRules" required />
+              </div>
+              <div class="form-group">
+                <label>Oggetto (facoltativo)</label>
+                <div class="checkbox-group">
+                  <label class="checkbox-label">
+                    <input type="checkbox" v-model="formData.appointmentRequest">
+                    Richiesta appuntamento
+                  </label>
+                  <label class="checkbox-label">
+                    <input type="checkbox" v-model="formData.questionRequest">
+                    Vorrei farti una domanda...
+                  </label>
+                </div>
+              </div>
+              <div class="form-group">
+                <label for="message">Il tuo messaggio</label>
+                <v-textarea v-model="formData.message" label="Messaggio" required rows="4" />
+              </div>
+              <v-btn type="submit" :disabled="!isValid" color="primary">INVIA</v-btn>
+            </v-form>
           </div>
-          <div class="form-group">
-            <label for="email">La tua email</label>
-            <v-text-field 
-              v-model="formData.email" 
-              label="Email" 
-              :rules="validation.emailRules" 
-              required 
-            />
-          </div>
-          <div class="form-group">
-            <label>Oggetto (facoltativo)</label>
-            <div class="checkbox-group">
-              <label class="checkbox-label">
-                <input type="checkbox" v-model="formData.appointmentRequest">
-                Richiesta appuntamento
-              </label>
-              <label class="checkbox-label">
-                <input type="checkbox" v-model="formData.questionRequest">
-                Vorrei farti una domanda...
-              </label>
+        </v-col>
+        <v-col cols="12" :md="6">
+          <div class="contact-info">
+            <div class="contact-links">
+              <a href="tel:+393242425380" class="contact-link">
+                <v-icon class="contact-icon">mdi-phone</v-icon>
+                Chiamami
+              </a>
+              <a href="https://wa.me/+393425644546" class="contact-link">
+                <v-icon class="contact-icon">mdi-whatsapp</v-icon>
+                Contattami su WhatsApp
+              </a>
+            </div>
+            <div class="contact-details">
+              <h3>Indirizzo</h3>
+              <p>Via Corfù, 13</p>
+              <p>70121 Bari</p>
+              <h3>Orario studio</h3>
+              <p>Dal lunedì al sabato solo su appuntamento</p>
             </div>
           </div>
-          <div class="form-group">
-            <label for="message">Il tuo messaggio</label>
-            <v-textarea 
-              v-model="formData.message" 
-              label="Messaggio" 
-              required 
-              rows="4" 
-            />
-          </div>
-          <v-btn type="submit" :disabled="!isValid" color="primary">INVIA</v-btn>
-        </v-form>
-      </div>
-      <div class="contact-info">
-        <div class="contact-links">
-          <a href="tel:+393242425380" class="contact-link">
-            <v-icon class="contact-icon">mdi-phone</v-icon>
-            Chiamami
-          </a>
-          <a href="https://wa.me/+393425644546" class="contact-link">
-            <v-icon class="contact-icon">mdi-whatsapp</v-icon>
-            Contattami su WhatsApp
-          </a>
-        </div>
-        <div class="contact-details">
-          <h3>Indirizzo</h3>
-          <p>Via Corfù, 13</p>
-          <p>70121 Bari</p>
-          <h3>Orario studio</h3>
-          <p>Dal lunedì al sabato solo su appuntamento</p>
-        </div>
-      </div>
-    </div>
+        </v-col>
+      </v-row>
+    </v-container>
   </section>
 </template>
 
@@ -97,6 +78,9 @@
 import { ref, reactive } from 'vue';
 import validation from '@/utils/validation.js';
 import http from '@/utils/http.js';
+import mobile from '@/utils/mobile.js';
+
+const isMobile = mobile.setupMobileUtils();
 
 const isValid = ref(false);
 const form = ref(null);
@@ -114,9 +98,9 @@ const handleSubmit = () => {
       email: import.meta.env.VITE_FORM_MAIL,
       subject: "Nuovo contatto dal form del sito",
       body: `Nome: ${formData.nome}\nEmail: ${formData.email}\n` +
-            `Richiesta appuntamento: ${formData.appointmentRequest ? 'Sì' : 'No'}\n` +
-            `Domanda: ${formData.questionRequest ? 'Sì' : 'No'}\n` +
-            `Messaggio: ${formData.message}`
+        `Richiesta appuntamento: ${formData.appointmentRequest ? 'Sì' : 'No'}\n` +
+        `Domanda: ${formData.questionRequest ? 'Sì' : 'No'}\n` +
+        `Messaggio: ${formData.message}`
     };
     http.postRequest('send-mail', mailData, () => {
       alert("Mail inviata con successo! Ti ringraziamo per averci contattato.");
@@ -130,13 +114,13 @@ const handleSubmit = () => {
 <style scoped>
 .social-container {
   display: flex;
-  justify-content: center; 
+  justify-content: center;
   margin-bottom: 2rem;
-  margin-top: -3rem; 
+  margin-top: -3rem;
 }
 
 .social-buttons {
-  gap: 1.5rem; 
+  gap: 1.5rem;
 }
 
 .social-btn {
@@ -146,19 +130,6 @@ const handleSubmit = () => {
 
 .social-btn:hover {
   transform: scale(1.1);
-}
-
-.contact-section {
-  padding: 4rem 2rem;
-  background-color: #faf6f1;
-}
-
-.contact-container {
-  max-width: 1200px;
-  margin: 0 auto;
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 4rem;
 }
 
 .form-title {
@@ -175,10 +146,6 @@ const handleSubmit = () => {
   display: block;
   color: #8B4513;
   margin-bottom: 0.5rem;
-}
-
-.contact-info {
-  padding: 2rem;
 }
 
 .contact-links {
