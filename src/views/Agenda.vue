ac7c030640b3b27ea3e418b7e7fc6ef2fef3cea1
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2025-03-21 21:34:27 +0000

    Blog

--- src/views/Agenda.vue
+++ src/views/Agenda.vue
@@ -1,51 +1,63 @@
 <template>
   <v-container>
-    <v-row v-if="!isMobile">
-      <v-col cols="8">
-        <AgendaItem :post="posts[0]" :height="600" />
-      </v-col>
-      <v-col cols="4">
-        <AgendaItem :post="posts[1]" :height="200" />
-        <AgendaItem :post="posts[2]" :height="200" />
-      </v-col>
-    </v-row>
-    <v-row v-if="!isMobile">
-      <v-col cols="4" v-for="post in posts.slice(3)">
-        <AgendaItem :post="post" :height="200" />
-      </v-col>
-    </v-row>
-    <v-row v-else>
-      <v-col cols="12" md="12" v-for="post in posts">
-        <AgendaItem :post="post" />
-        <hr class="dotted-hr red" v-if="isMobile && posts.indexOf(post) != posts.length - 1">
-      </v-col>
-    </v-row>
+    <h1 style="font-family: 'Montserrat', sans-serif; text-transform: uppercase; margin: 10px 0;">Ultimi post</h1>
+    <div v-for="(post, index) in displayedPosts" :key="post.id">
+      <AgendaItem :post="post" :isFeatured="index === 0"/>
+    </div>
+    <div v-if="displayedPosts.length < posts.length" class="text-center mt-4">
+      <a @click="loadMorePosts" class="more-posts">Mostra più articoli</a>
+    </div>
   </v-container>
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { ref, computed } from 'vue';
 import http from '@/utils/http';
 import mobile from '@/utils/mobile';
-
 import AgendaItem from '@/components/AgendaItem';
 
 const posts = ref([]);
 const isMobile = mobile.setupMobileUtils();
+const itemsToShow = ref(3);
 
 http.getRequest('blog/post', {
   project: 'dorianadinanni.it'
 }, function (data) {
-  posts.value = data.posts;
+  posts.value = data.posts.reverse();
 });
+
+const displayedPosts = computed(() => posts.value.slice(0, itemsToShow.value));
+
+const loadMorePosts = () => {
+  itemsToShow.value += 5;
+};
 </script>
 
 <style scoped>
-.dotted-hr {
-  border: 3px dotted;
-  border-top: none;
-  height: 0;
-  margin: 20px 0;
+.more-posts {
+  font-family: 'Montserrat', sans-serif;
+  cursor: pointer;
+  text-transform: uppercase;
+  font-weight: 600;
+  text-decoration: none;
+  position: relative;
+  display: inline-block;
+}
+
+.more-posts::after {
+  content: '';
+  position: absolute;
+  left: 0%;
+  bottom: -2px;
+  width: 50%;
+  height: 2px;
+  background-color: currentColor;
+  transition: width 0.3s ease, left 0.3s ease;
+}
+
+.more-posts:hover::after {
   width: 100%;
+  left: 0;
+  transform: none;
 }
 </style>
