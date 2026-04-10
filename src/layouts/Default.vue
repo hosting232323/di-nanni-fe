51198a5eb3549639162106a826d18924926eafc1
cl_cassanelli <80552052+cl-cassanelli@users.noreply.github.com>
2026-04-10 15:51:04 +0200

    push

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -17,38 +17,9 @@ import AppBar from './AppBar.vue';
 import Footer from './Footer.vue';
 
 import { ChattyBot } from 'generic-module';
+import { botLanding } from '@/utils/chatty';
 
 const viteHostnameChattyBe = import.meta.env.VITE_HOSTNAME_CHATTY_BE;
-
-const colorPalette = {
-  theme_color: '#7d2636',
-  theme_color_hover: '#a6344c',
-  fab_hover: '#912c3f',
-  fab_shadow: '#b35a6b',
-  fab_border: '#a6344c'
-};
-
-const botLanding = {
-  id: 2,
-  message: 'Ciao, come posso aiutarti.',
-  name: 'dott. ssa Doriana Di Nanni',
-  image: '/logo.png',
-  faq: [
-    {
-      'name': 'Fa male? 😌',
-      'value': 'L\'agopuntura è dolorosa?'
-    },
-    {
-      'name': 'Quante sedute servono? ⏳',
-      'value': 'Di quante sedute ho bisogno per vedere risultati?'
-    },
-    {
-      'name': 'È sicura? 🌿',
-      'value': 'L\'uso delle piante medicinali può avere effetti indesiderati?'
-    }
-  ],
-  color: colorPalette,
-};
 </script>
 
 <style>
---
+++ src/utils/chatty.js
@@ -0,0 +1,30 @@
+const colorPalette = {
+  theme_color: '#7d2636',
+  theme_color_hover: '#a6344c',
+  fab_hover: '#912c3f',
+  fab_shadow: '#b35a6b',
+  fab_border: '#a6344c'
+};
+
+export const botLanding = {
+  vectorStoreId: 'vs_69d8ff08c87c8191bdd023226e21d586',
+  message: 'Ciao, come posso aiutarti.',
+  name: 'dott. ssa Doriana Di Nanni',
+  image: '/logo.png',
+  faq: [
+    {
+      'name': 'Fa male? 😌',
+      'value': 'L\'agopuntura è dolorosa?'
+    },
+    {
+      'name': 'Quante sedute servono? ⏳',
+      'value': 'Di quante sedute ho bisogno per vedere risultati?'
+    },
+    {
+      'name': 'È sicura? 🌿',
+      'value': 'L\'uso delle piante medicinali può avere effetti indesiderati?'
+    }
+  ],
+  stream: true,
+  color: colorPalette,
+};
