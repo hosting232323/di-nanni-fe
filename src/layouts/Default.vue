1faaea04c6245f25fff980815ab291a5727752a5
Vanni-broUser <colasanto.giovanni.inf@gmail.com>
2026-05-15 17:36:47 +0200

    fix finali

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -13,12 +13,40 @@
 </template>
 
 <script setup>
-import http from '@/utils/http';
 import AppBar from './AppBar.vue';
 import Footer from './Footer.vue';
-
 import { ChattyBot } from 'generic-module';
-import { botLanding } from '@/utils/chatty';
+
+import http from '@/utils/http';
+
+const botLanding = {
+  botId: 6,
+  message: 'Ciao, come posso aiutarti.',
+  name: 'dott. ssa Doriana Di Nanni',
+  image: '/logo.png',
+  faq: [
+    {
+      'name': 'Fa male? 😌',
+      'value': 'L\'agopuntura è dolorosa?'
+    },
+    {
+      'name': 'Quante sedute servono? ⏳',
+      'value': 'Di quante sedute ho bisogno per vedere risultati?'
+    },
+    {
+      'name': 'È sicura? 🌿',
+      'value': 'L\'uso delle piante medicinali può avere effetti indesiderati?'
+    }
+  ],
+  color: {
+    theme_color: '#7d2636',
+    theme_color_hover: '#a6344c',
+    fab_hover: '#912c3f',
+    fab_shadow: '#b35a6b',
+    fab_border: '#a6344c'
+  },
+  stream: true
+};
 </script>
 
 <style>
